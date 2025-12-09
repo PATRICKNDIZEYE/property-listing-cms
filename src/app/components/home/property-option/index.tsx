fd8bfd8b8fb6b5b250d2c29b261509989f7a9f12
kitigroup <kitigroup.co@gmail.com>
2025-12-09 20:25:40 +0200

    adding hovered on discover property

--- src/app/components/home/property-option/index.tsx
+++ src/app/components/home/property-option/index.tsx
@@ -41,7 +41,7 @@ export default function DiscoverProperties() {
 
 
     return (
-        <section className='dark:bg-darkmode'>
+        <section className='bg-light dark:bg-semidark'>
             <div className="container lg:max-w-screen-xl md:max-w-screen-md mx-auto px-4">
                 <h2 className="text-4xl font-bold mb-12 text-midnight_text dark:text-white" data-aos="fade-left">Discover Properties</h2>
                 <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 lg:gap-4 gap-8">
@@ -51,12 +51,12 @@ export default function DiscoverProperties() {
                                 <Image
                                     src={property.category_img}
                                     alt="Image"
-                                    className='p-4 border-2 rounded-lg border-border dark:border-dark_border mb-6 group-hover:-translate-y-1 group-hover:duration-500'
+                                    className='p-4 border-2 rounded-lg border-border dark:border-dark_border mb-6 group-hover:-translate-y-1 group-hover:duration-500 group-hover:border-primary dark:group-hover:border-primary transition-colors'
                                     height={85}
                                     width={85}
                                 />
-                                <p className="text-[22px] leading-[1.2] font-semibold mt-2 text-midnight_text text-opacity-80 group-hover:text-opacity-100 dark:text-white dark:group-hover:text-white dark:group-hover:text-opacity-100 dark:text-opacity-70 mb-1 capitalize">{property.category}</p>
-                                <p className="text-base text-gray">{property.count} Properties</p>
+                                <p className="text-[22px] leading-[1.2] font-semibold mt-2 text-midnight_text text-opacity-80 group-hover:text-primary group-hover:text-opacity-100 dark:text-white dark:group-hover:text-primary dark:group-hover:text-opacity-100 dark:text-opacity-70 mb-1 capitalize transition-colors">{property.category}</p>
+                                <p className="text-base text-gray group-hover:text-primary dark:group-hover:text-primary transition-colors">{property.count} Properties</p>
                             </Link>
                         </div>
                     ))}
